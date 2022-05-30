b1072873422d0abe494eab0e317af237a47177bf
Jake Lee <freejak5520@gmail.com>
2022-05-30 12:07:46 +0900

    의존관계 주입(DI) 적용

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -1,5 +1,6 @@
 package hello.core;
 
+import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
@@ -11,11 +12,13 @@ import hello.core.order.OrderServiceImpl;
 public class OrderApp {
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
+
         memberService.join(member);
 
         Order order = orderService.createOrder(memberId, "itemA", 10000);
